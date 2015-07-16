6691ffd03b025d5f19c74ec0bb954c28cc0c8471
Arman13 <armani137@mail.ru>
2015-07-16 12:51:57 +0400

    Event

---
+++ Event/Printer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Event
+{
+    public class Printer
+    {
+        public event EventHandler PrintStarted;
+        public event EventHandler PrintFinished;
+        public event EventHandler<PrintingEventArgs> Printing;
+        public void Print(int pageCount)
+        {
+            OnPrintStarted();
+            for (int i = 1; i <= pageCount; i++)
+            {
+                OnPrinting(i);
+                Thread.Sleep(1000);
+            }
+            OnPrintFinishd();
+        }
+        private void OnPrinting(int i)
+        {
+
+            if (PrintFinished != null)
+            {
+                EventHandler<PrintingEventArgs> del = (EventHandler<PrintingEventArgs>)Printing;
+                PrintingEventArgs args = new PrintingEventArgs();
+                args.CurrentPageNumber = i;
+                del(this, args);
+            }
+        }
+        private void OnPrintFinishd()
+        {
+            if (PrintStarted != null)
+            {
+                EventHandler del = (EventHandler)PrintFinished;
+                del(this, EventArgs.Empty);
+            }
+        }
+        private void OnPrintStarted()
+        {
+            if (PrintStarted != null)
+            {
+                EventHandler del = (EventHandler)PrintStarted;
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Event/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var printer = new Printer();
+            printer.PrintStarted += printer_PrintStarted;
+            printer.Printing += printer_Printing;
+            printer.PrintFinished += printer_PrintFinished;
+
+            printer.Print(4);
+
+        }
+        static void printer_PrintFinished(object sender, EventArgs e)
+        {
+            Console.WriteLine("Print Finished");
+        }
+        static void printer_Printing(object sender, PrintingEventArgs e)
+        {
+            Console.WriteLine(e.CurrentPageNumber);
+        }
+        static void printer_PrintStarted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Print Started");
+        }
+    }
+}
